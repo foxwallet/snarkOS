e9bb28948431d915300839fafeb56e890ef20936
zhangxianxing <xianxing.zhang1994@gmail.com>
2023-07-07 15:24:26 +0800

    test

--- node/rest/src/routes.rs
+++ node/rest/src/routes.rs
@@ -162,7 +162,7 @@ impl<N: Network, C: ConsensusStorage<N>, R: Routing<N>> Rest<N, C, R> {
         State(rest): State<Self>,
         Path(commitment): Path<Field<N>>,
     ) -> Result<ErasedJson, RestError> {
-        Ok(ErasedJson::pretty(rest.ledger.get_state_path_for_commitment(&commitment)?))
+        Ok(ErasedJson::pretty(rest.ledger.&commitment)?))
     }
 
     // GET /testnet3/beacons
@@ -273,13 +273,13 @@ impl<N: Network, C: ConsensusStorage<N>, R: Routing<N>> Rest<N, C, R> {
         Ok(ErasedJson::pretty(rest.ledger.find_record_from_commitment(&commitment)?))
     }
 
-    pub(crate) async fn get_plaintext_from_commitment(State(rest): State<Self>, Path(commitment): Path<Field<N>>, Path(view_key): Path<ViewKey<N>>) -> Result<ErasedJson, RestError> {
+    // pub(crate) async fn get_plaintext_from_commitment(State(rest): State<Self>, Path(commitment): Path<Field<N>>, Path(view_key): Path<ViewKey<N>>) -> Result<ErasedJson, RestError> {
         // let records: IndexMap<_, _> = rest.ledger.find_records(&view_key, RecordsFilter::Unspent).unwrap().collect();
-        Ok(ErasedJson::pretty(rest.ledger.decrypt_record_from_commitment(&commitment, &view_key)?))
-    }
+        // Ok(ErasedJson::pretty(rest.ledger.decrypt_record_from_commitment(&commitment, &view_key)?))
+    // }
 
-    pub(crate) async fn check_record_spent(State(rest): State<Self>, Path(commitment): Path<Field<N>>, Path(view_key): Path<ViewKey<N>>) -> Result<ErasedJson, RestError> {
+    // pub(crate) async fn check_record_spent(State(rest): State<Self>, Path(commitment): Path<Field<N>>, Path(view_key): Path<ViewKey<N>>) -> Result<ErasedJson, RestError> {
         // let records: IndexMap<_, _> = rest.ledger.find_records(&view_key, RecordsFilter::Unspent).unwrap().collect();
-        Ok(ErasedJson::pretty(rest.ledger.check_record_spent(&commitment, &view_key)?))
-    }
+        // Ok(ErasedJson::pretty(rest.ledger.check_record_spent(&commitment, &view_key)?))
+    // }
 }
